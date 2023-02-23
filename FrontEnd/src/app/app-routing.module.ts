0ecc160a8d952b993735d0e70651ae8a34c1ad1c
kariuki-john <jnk4131@gmail.com>
2023-02-23 14:38:11 +0300

    Go ahead

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ViewChild } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './Users/about-us/about-us.component';
+import { CartComponent } from './Users/cart/cart.component';
+import { ContactUsComponent } from './Users/contact-us/contact-us.component';
+import { HomeComponent } from './Users/home/home.component';
+import { LoginComponent } from './Users/login/login.component';
+import { LogoutComponent } from './Users/logout/logout.component';
+import { ProductsComponent } from './Users/products/products.component';
+import { RegisterComponent } from './Users/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"products",component:ProductsComponent},
+  {path:"aboutUs",component:AboutUsComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"contactUs",component:ContactUsComponent},
+  {path:"login",component:LoginComponent},
+  {path:"cart",component:CartComponent},
+  {path:"logout",component:LogoutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
